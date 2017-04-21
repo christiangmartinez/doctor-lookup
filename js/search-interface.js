22e882c5b44af5e2079ba9bf114d4a4ef0a19a5a
Christian Martinez <cgmartinez11142@gmail.com>
2017-04-21 16:24:17 -0700

    add functionality to show individual doctor info on page

--- js/search-interface.js
+++ js/search-interface.js
@@ -1,21 +1,26 @@
 var Search = require('./../js/search.js').searchModule;
+var currentSearchObject = new Search();
 
 var showDocs = function(docs) {
-  $('#docs').empty()
+  $('#docs').empty();
   docs.forEach(function(doc) {
     $('#docs').append('<li><h4 id="' + doc.uid + '" class="docName">' + doc.profile.first_name + ' ' + doc.profile.last_name + ', ' + doc.profile.title + '</h4><img src="' + doc.profile.image_url + '"alt="' + doc.profile.slug + '"></li>');
   });
 }
 
+var showProfile = function(profile) {
+  $('#docs').empty();
+  $('#docs').append('<h2>' + profile.first_name + ' ' + profile.last_name + ', ' + profile.title + '</h2>');
+}
+
 $(document).ready(function() {
   $('#docsBySymptom').submit(function() {
     event.preventDefault();
-    var currentSearchObject = new Search();
     var userSymptom = $('#userSymptom').val();
     currentSearchObject.getDocsBySymptom(userSymptom, showDocs);
   });
   $(document).on('click', '.docName', function() {
     var uid = $(this).attr('id');
-    console.log(uid);
+    currentSearchObject.getDocProfile(uid, showProfile);
   });
 });
--- js/search.js
+++ js/search.js
@@ -8,7 +8,6 @@ Search.prototype.getDocsBySymptom = function(userSymptom, showDocs) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ userSymptom +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
      var docs = result.data;
-     console.log(result);
      showDocs(docs);
     })
    .fail(function(error) {
@@ -16,10 +15,12 @@ Search.prototype.getDocsBySymptom = function(userSymptom, showDocs) {
     });
 }
 
-Search.prototype.getDocProfile = function (uid) {
+Search.prototype.getDocProfile = function (uid, showProfile) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors/' + uid +'?user_key=' + apiKey)
   .then(function(result) {
-    console.log(result);
+    var profile = result.data.profile;
+    showProfile(profile);
+    console.log(profile);
   })
   .fail(function(error) {
     console.log('ERROR');
