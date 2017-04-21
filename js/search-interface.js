9a47b5c8babfa4a4a3baef57456d613ce10824d6
Christian Martinez <cgmartinez11142@gmail.com>
2017-04-21 12:14:45 -0700

    modify submit for succesful api call

--- js/search-interface.js
+++ js/search-interface.js
@@ -3,6 +3,8 @@ var Search = require('./../js/search.js').searchModule;
 $(document).ready(function() {
   $('#docsBySymptom').submit(function() {
     event.preventDefault();
+    var currentSearchObject = new Search;
     var userSymptom = $('#userSymptom').val();
+    currentSearchObject.getDoctors(userSymptom);
   });
 });
