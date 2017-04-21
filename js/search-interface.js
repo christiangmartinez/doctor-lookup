167003812d620ab73d35d35804979739b3e9f02d
Christian Martinez <cgmartinez11142@gmail.com>
2017-04-21 13:15:46 -0700

    add function to display doctor profile image with name and title for each doctor in result

--- js/search-interface.js
+++ js/search-interface.js
@@ -1,10 +1,16 @@
 var Search = require('./../js/search.js').searchModule;
 
+var showDocs = function(docs) {
+  docs.forEach(function(doc) {
+    $('#docs').append('<li> <img src="' + doc.profile.image_url + '"alt="' + doc.profile.slug + '"><h4>' + doc.profile.first_name + ' ' + doc.profile.last_name + ', ' + doc.profile.title + '</h4></li>');
+  });
+}
+
 $(document).ready(function() {
   $('#docsBySymptom').submit(function() {
     event.preventDefault();
-    var currentSearchObject = new Search;
+    var currentSearchObject = new Search();
     var userSymptom = $('#userSymptom').val();
-    currentSearchObject.getDoctors(userSymptom);
+    currentSearchObject.getDocsBySymptom(userSymptom, showDocs);
   });
 });
--- js/search.js
+++ js/search.js
@@ -4,10 +4,12 @@ Search = function(){
 
 }
 
-Search.prototype.getDoctors = function(userSymptom) {
+Search.prototype.getDocsBySymptom = function(userSymptom, showDocs) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ userSymptom +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
-      console.log(result);
+     var docs = result.data;
+     console.log(result);
+     showDocs(docs);
     })
    .fail(function(error) {
       console.log("fail");
